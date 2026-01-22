fa8e87033de3184ccf23171f7d4c66d4856e4edd
Mapkyc18 <158997362+Mapkyc18@users.noreply.github.com>
2026-01-22 09:47:43 -0500

    Update calculate_product.h

--- calculate_product.h
+++ calculate_product.h
@@ -1,6 +1,9 @@
 #ifndef CALCULATE_SUM_H
 #define CALCULATE_SUM_H
 
+// Function prototype for calculate_product.
+// This lets other files know the function exists
+// before its actual implementation is used.
 int calculate_product(int a, int b);
 
 #endif
