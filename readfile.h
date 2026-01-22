64507088146e0495f6e4ae584f06db7559471dfb
Mapkyc18 <158997362+Mapkyc18@users.noreply.github.com>
2026-01-22 09:50:01 -0500

    Update readfile.h comment

--- readfile.h
+++ readfile.h
@@ -1,6 +1,9 @@
 #ifndef READFILE_H
 #define READFILE_H
 
+// Function prototype for readfile.
+// This function is responsible for reading data from a file.
+// The details of how the file is read are handled in the source file.
 void readfile();
 
 #endif
