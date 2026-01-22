bb6aba9d94fce613e8637708d025065e976f9140
Mapkyc18 <158997362+Mapkyc18@users.noreply.github.com>
2026-01-22 09:50:59 -0500

    Update writefile.h cooment

--- writefile.h
+++ writefile.h
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#ifndef MYCODE_H 
-#define MYCODE_H 
-// Declaration 
 
+#ifndef MYCODE_H
+#define MYCODE_H
+
+// Function declaration for writefile.
+// This tells the compiler that the function exists
+// and will be defined somewhere else in the program.
 void writefile();
 
 #endif // MYCODE_H
