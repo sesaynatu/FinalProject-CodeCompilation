686e0a12329541c5e90a3ca65f2c743807d00940
Mapkyc18 <158997362+Mapkyc18@users.noreply.github.com>
2026-01-22 09:49:30 -0500

    Update calculate_sum.h comment

--- calculate_sum.h
+++ calculate_sum.h
@@ -1,6 +1,9 @@
 #ifndef CALCULATE_SUM_H
 #define CALCULATE_SUM_H
 
+// Function prototype for calculate_sum.
+// This allows other files to use the function
+// without needing to know how it’s implemented.
 int calculate_sum(int a, int b);
 
 #endif
