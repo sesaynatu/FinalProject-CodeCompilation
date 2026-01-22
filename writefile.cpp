880982d6b863f0c24394b88cf0ee986c7d5cb19f
Mapkyc18 <158997362+Mapkyc18@users.noreply.github.com>
2026-01-22 09:50:36 -0500

    Update writefile.cpp comment

--- writefile.cpp
+++ writefile.cpp
@@ -1,17 +1,29 @@
 #include <stdio.h>
-void writefile() {
-   FILE *fp;   
-    
-    
+
+// This function creates a file and writes text into it.
+// It shows a basic example of file writing in C.
+void writefile()
+{
+    FILE *fp;   // File pointer used to access the file
+
+    // Open the file in write mode (creates it if it doesn't exist)
     fp = fopen("test.txt", "w+");
-   fprintf(fp, "This is testing for fprintf...\n");
-   fputs("This is testing for fputs...\n", fp);
-   fclose(fp);
-   printf("Successfully wrote test.txt");
-};
+
+    // Write text to the file using two different methods
+    fprintf(fp, "This is testing for fprintf...\n");
+    fputs("This is testing for fputs...\n", fp);
+
+    // Close the file once writing is finished
+    fclose(fp);
+
+    // Confirm to the user that the file was written
+    printf("Successfully wrote test.txt");
+}
 
 int main()
 {
-  writefile();
-}
+    // Call the writefile function to create and write to the file
+    writefile();
 
+    return 0;
+}
